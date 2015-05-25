b867438364ec0791b1e7b8a6240f71ea3edd510e
Lukas Elmer <lukas.elmer@gmail.com>
2015-05-25 20:00:50 +0200

    use more interesting numbers

--- rustapp.rb
+++ rustapp.rb
@@ -3,8 +3,8 @@ require 'ffi'
 require_relative 'app/fast_calculator'
 
 get '/' do
-  num1 = (params['num1'] || 1.0).to_f
-  num2 = (params['num2'] || 2.0).to_f
+  num1 = (params['num1'] || 3.0).to_f
+  num2 = (params['num2'] || 39.0).to_f
   result = FastCalculator.plus(num1, num2)
   "<form><input name='num1' value='#{num1}' autofocus> + <input name='num2' value='#{num2}'> = #{result} <input type='submit'></form>"
 end
